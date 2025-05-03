8da5018e69c14833cba776988e6f56b5b620165f
Fauzan <fauzancoding@gmail.com>
2025-05-03 15:22:49 +0700

    E

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
-// backend/routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/userController');
 
+// Register route
 router.post('/register', userCtrl.register);
+
+// Login route
 router.post('/login', userCtrl.login);
 
 module.exports = router;
